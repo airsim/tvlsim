8ab2f3423aedacde0d48aee25b37f89790486110
Denis Arnaud <denis@users.sourceforge.net>
2010-09-07 11:30:40 +0000

    [Build] The StdAir and ExtraCC projects are no longer Subversion external references.
    git-svn-id: https://dsim.svn.sourceforge.net/svnroot/dsim/trunk@124 e67a024f-d144-4c12-b96e-b057add9d369

--- dsim/dsim/DSIM_Service.hpp
+++ dsim/dsim/DSIM_Service.hpp
@@ -1,113 +1,242 @@
-#ifndef __DSIM_SVC_DSIM_SERVICE_HPP
-#define __DSIM_SVC_DSIM_SERVICE_HPP
+/* dsim/DSIM_Service.hpp.  Generated from DSIM_Service.hpp.in by configure.  */
+/* dsim/DSIM_Service.hpp.in.  Generated from configure.ac by autoheader.  */
 
-// //////////////////////////////////////////////////////////////////////
-// Import section
-// //////////////////////////////////////////////////////////////////////
-// StdAir
-#include <stdair/STDAIR_Types.hpp>
-// Dsim
-#include <dsim/DSIM_Types.hpp>
 
-// Forward declarations.
-namespace stdair {
-  class AirlineFeatureSet;
-  class STDAIR_Service;
-  struct BasLogParams;
-  struct BasDBParams;
-}
+#ifndef CONFIG_H
+#define CONFIG_H
 
-namespace DSIM {
 
-  // Forward declaration
-  class DSIM_ServiceContext;
-  struct RDSParameters;
-  
-  /** Interface for the DSIM Services. */
-  class DSIM_Service {
-  public:
-    // ////////// Constructors and destructors //////////
-    /** Constructor.
-        <br>The init() method is called; see the corresponding documentation
-        for more details.
-        <br>A reference on an output stream is given, so that log
-        outputs can be directed onto that stream.
-        <br>Moreover, database connection parameters are given, so that a
-        session can be created on the corresponding database.
-        @param const stdair::BasLogParams& Parameters for the output log stream.
-        @param const stdair::BasDBParams& Parameters for the database access.
-        @param const stdair::Filename_T& Filename of the input schedule file.
-        @param const stdair::Filename_T& Filename of the input demand file. */
-    DSIM_Service (const stdair::BasLogParams&, const stdair::BasDBParams&,
-                  const stdair::Filename_T& iScheduleInputFilename,
-                  const stdair::Filename_T& iDemandInputFilenames);
+/* Define if you want exceptions handling */
+/* #undef DSIM_EXCEPTIONS */
 
-    /** Constructor.
-        <br>The init() method is called; see the corresponding documentation
-        for more details.
-        <br>Moreover, as no reference on any output stream is given,
-        neither any database access parameter is given, it is assumed
-        that the StdAir log service has already been initialised with
-        the proper log output stream by some other methods in the
-        calling chain (for instance, when the DSIM_Service is
-        itself being initialised by another library service).
-        @param const stdair::Filename_T& Filename of the input schedule file.
-        @param const stdair::Filename_T& Filename of the input demand file. */
-    DSIM_Service (stdair::STDAIR_ServicePtr_T,
-                  const stdair::Filename_T& iScheduleInputFilename,
-                  const stdair::Filename_T& iDemandInputFilenames);
+/* define if the Boost library is available */
+#define HAVE_BOOST /**/
 
-    /** Destructor. */
-    ~DSIM_Service();
+/* define if the Boost::Asio library is available */
+#define HAVE_BOOST_ASIO /**/
 
-    
-  public:
-    // /////////// Business Methods /////////////
-    /** Perform a simulation. */
-    void simulate();
-    
-    /** Display the list of airlines. */
-    void displayAirlineListFromDB() const;
+/* define if the Boost::Date_Time library is available */
+#define HAVE_BOOST_DATE_TIME /**/
 
-    
-  private:
-    // /////// Construction and Destruction helper methods ///////
-    /** Default constructor. */
-    DSIM_Service ();
-    /** Default copy constructor. */
-    DSIM_Service (const DSIM_Service&);
+/* define if the Boost::FILESYSTEM library is available */
+#define HAVE_BOOST_FILESYSTEM /**/
 
-    /** Initialise the (DSIM) service context (i.e., the
-        DSIM_ServiceContext object). */
-    void initServiceContext ();
+/* define if the Boost::IOStreams library is available */
+#define HAVE_BOOST_IOSTREAMS /**/
 
-    /** Initialise the STDAIR service (including the log service).
-        <br>A reference on the root of the BOM tree, namely the BomRoot object,
-        is stored within the service context for later use.
-        @param const stdair::BasLogParams& Parameters for the output log stream.
-        @param const stdair::BasDBParams& Parameters for the database access.
-        @param const stdair::AirlineFeatureSet& Set of airline features. */
-    void initStdAirService (const stdair::BasLogParams&,
-                            const stdair::BasDBParams&);
-    
-    /** Initialise.
-        <br>The CSV file, describing the airline schedules for the
-        simulator, is parsed and the inventories are generated accordingly.
-        @param const stdair::AirlineFeatureSet& Set of airline features.
-        @param const stdair::Filename_T& Filename of the input schedule file.
-        @param const stdair::Filename_T& Filename of the input demand file. */
-    void init (const stdair::Filename_T& iScheduleInputFilename,
-               const stdair::Filename_T& iDemandInputFilename);
-    
-    /** Finalise. */
-    void finalise ();
+/* define if the Boost::Mpi library is available */
+#define HAVE_BOOST_MPI /**/
 
-    
-  private:
-    // ///////// Service Context /////////
-    /** Dsim context. */
-    DSIM_ServiceContext* _dsimServiceContext;
+/* define if the Boost::Mpi_Python library is available */
+#define HAVE_BOOST_MPI_PYTHON /**/
 
-  };
-}
-#endif // __DSIM_SVC_DSIM_SERVICE_HPP
+/* define if the Boost::PROGRAM_OPTIONS library is available */
+#define HAVE_BOOST_PROGRAM_OPTIONS /**/
+
+/* define if the Boost::Regex library is available */
+#define HAVE_BOOST_REGEX /**/
+
+/* define if the Boost::Serialization library is available */
+#define HAVE_BOOST_SERIALIZATION /**/
+
+/* define if the Boost::Signals library is available */
+#define HAVE_BOOST_SIGNALS /**/
+
+/* define if the Boost::SYSTEM library is available */
+#define HAVE_BOOST_SYSTEM /**/
+
+/* define if the Boost::THREAD library is available */
+#define HAVE_BOOST_THREAD /**/
+
+/* define if the Boost::Unit_test_framework library is available */
+#define HAVE_BOOST_UNIT_TEST_FRAMEWORK /**/
+
+/* Define to 1 if you have the <cmath> header file. */
+#define HAVE_CMATH 1
+
+/* Define to 1 if you have the declaration of `signgam', and to 0 if you
+   don't. */
+#define HAVE_DECL_SIGNGAM 1
+
+/* Define to 1 if you have the <deque> header file. */
+#define HAVE_DEQUE 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `erf' function. */
+#define HAVE_ERF 1
+
+/* Define to 1 if you have the `erfc' function. */
+#define HAVE_ERFC 1
+
+/* Define to 1 if you have the `finite' function. */
+#define HAVE_FINITE 1
+
+/* Define to 1 if you have the `fpclass' function. */
+/* #undef HAVE_FPCLASS */
+
+/* Define to 1 if you have the <fstream> header file. */
+#define HAVE_FSTREAM 1
+
+/* Define to 1 if you have the <ieeefp.h> header file. */
+/* #undef HAVE_IEEEFP_H */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <iomanip> header file. */
+#define HAVE_IOMANIP 1
+
+/* Define to 1 if you have the <iostream> header file. */
+#define HAVE_IOSTREAM 1
+
+/* Define to 1 if you have the `isfinite' function. */
+/* #undef HAVE_ISFINITE */
+
+/* Define to 1 if you have the `isinf' function. */
+#define HAVE_ISINF 1
+
+/* Define to 1 if you have the `isnan' function. */
+#define HAVE_ISNAN 1
+
+/* Define to 1 if you have the `lgamma' function. */
+#define HAVE_LGAMMA 1
+
+/* Define to 1 if you have the <limits> header file. */
+#define HAVE_LIMITS 1
+
+/* Define to 1 if you have the <list> header file. */
+#define HAVE_LIST 1
+
+/* Define to 1 if you have the `log1p' function. */
+#define HAVE_LOG1P 1
+
+/* Define to 1 if you have the <map> header file. */
+#define HAVE_MAP 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <queue> header file. */
+#define HAVE_QUEUE 1
+
+/* Define to 1 if you have the `rint' function. */
+#define HAVE_RINT 1
+
+/* Define to 1 if you have the <sstream> header file. */
+#define HAVE_SSTREAM 1
+
+/* Define to 1 if you have the <stdexcept> header file. */
+#define HAVE_STDEXCEPT 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <string> header file. */
+#define HAVE_STRING 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the `tgamma' function. */
+#define HAVE_TGAMMA 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <vector> header file. */
+#define HAVE_VECTOR 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "dsim"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "denis_arnaud@users.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "DSIM"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "DSIM 99.99.99"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "dsim"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "99.99.99"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Version number of package */
+#define VERSION "99.99.99"
+
+
+#if HAVE_CMATH
+#  include <cmath>
+#endif
+
+/* Solaris uses <ieeefp.h> for declaring isnan() and finite() functions */
+#if HAVE_IEEEFP_H
+#  include <ieeefp.h>
+#endif
+
+/* Microsoft Visual C++ .NET underscore prefixed functions */
+#if _MSC_VER
+#  include <cfloat>
+#  define HAVE_FINITE 1
+#  define finite(x) _finite(x)
+#  define HAVE_ISNAN 1
+#  define isnan(x) _isnan(x)
+#  define HAVE_FPCLASS 1
+#  define fpclass(x) _fpclass(x)
+#  define HAVE_JN 1
+#  define jn(a, b) _jn(a, b)
+#  define HAVE_YN 1
+#  define yn(a, b) _yn(a, b)
+#  define HAVE_J0 1
+#  define j0(a) _j0(a)
+#  define HAVE_J1 1
+#  define j1(a) _j1(a)
+#endif
+
+#if (! defined(HAVE_ISINF) && defined(HAVE_FPCLASS))
+#  define HAVE_ISINF 1
+#  define isinf(a) (fpclass(a) == FP_NINF || fpclass(a) == FP_PINF)
+#endif
+
+#if (! defined (HAVE_FINITE) && defined (HAVE_ISFINITE))
+#  define HAVE_FINITE 1
+#  define finite(a) isfinite(a)
+#endif
+
+#if (! defined(HAVE_FINITE) && defined(HAVE_ISNAN) && defined(HAVE_ISINF))
+#  define HAVE_FINITE 1
+#  define finite(a) (! isnan(a) && ! isinf(a))
+#endif
+
+#endif /* #ifndef CONFIG_H */
+
