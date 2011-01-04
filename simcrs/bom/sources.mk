f94f748f73785426c505965caed8a929c9d9c2e8
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-01-04 20:07:43 +0100

    [SimCRS][Build] Fixed a small typo in the BOM layer Makefile of the SimCRS module.

--- simcrs/bom/sources.mk
+++ simcrs/bom/sources.mk
@@ -1,2 +1,2 @@
-bom_h_sources = $(top_srcdir)/simcrs/bom/BomAbstract.hpp
-bom_cc_sources = $(top_srcdir)/simcrs/bom/BomAbstract.cpp
+simcrs_bom_h_sources = $(top_srcdir)/simcrs/bom/BomAbstract.hpp
+simcrs_bom_cc_sources = $(top_srcdir)/simcrs/bom/BomAbstract.cpp
