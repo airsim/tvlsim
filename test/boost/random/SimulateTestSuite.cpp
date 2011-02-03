898218a3a12bdef0b3edea1cda92c846414b3bea
Anh Quan Nguyen <quan.nguyen@amadeus.com>
2011-02-03 15:38:54 +0100

    [Test] Fixed a compilation error in Boost.Random test.

--- test/boost/random/SimulateTestSuite.cpp
+++ test/boost/random/SimulateTestSuite.cpp
@@ -1,6 +1,6 @@
 // STL
 #include <iostream>
-#include <sstream>
+#include <fstream>
 // Boost Date-Time
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
