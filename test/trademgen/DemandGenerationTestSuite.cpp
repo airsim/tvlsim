845b7bef83fc9b390f38bf3419caeaac1b0fabcf
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-02-04 20:00:38 +0100

    [TraDemGen][Test] The </TestLog> end tag is now appended to the XML result of the Boost.UTF tests.

--- test/trademgen/DemandGenerationTestSuite.cpp
+++ test/trademgen/DemandGenerationTestSuite.cpp
@@ -36,7 +36,8 @@ namespace boost_utf = boost::unit_test;
  */
 struct UnitTestConfig {
   /** Constructor. */
-  UnitTestConfig() : _test_log ("DemandGenerationTestSuite_utfresults.xml")  {
+  UnitTestConfig() {
+    static std::ofstream _test_log ("DemandGenerationTestSuite_utfresults.xml");
     boost_utf::unit_test_log.set_stream (_test_log);
     boost_utf::unit_test_log.set_format (boost_utf::XML);
     boost_utf::unit_test_log.set_threshold_level (boost_utf::log_test_units);
@@ -45,10 +46,7 @@ struct UnitTestConfig {
   
   /** Destructor. */
   ~UnitTestConfig() {
-    boost_utf::unit_test_log.set_stream (std::cout);
   }
-  /** Log file */  
-  std::ofstream _test_log;
 };
 
 // Specific type definitions
