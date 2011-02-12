d186ec553ed049cf8fb229b58a07d5124e2985fd
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-02-12 14:36:48 +0100

    [TraDemGen][Dev] Improved the logging output of the TraDemGen batch.

--- trademgen/batches/trademgen.cpp
+++ trademgen/batches/trademgen.cpp
@@ -1,16 +1,21 @@
 // STL
 #include <cassert>
-#include <iostream>
 #include <sstream>
 #include <fstream>
 #include <vector>
 #include <list>
 #include <string>
-// Boost (Extended STL)
+//  //// Boost (Extended STL) ////
+// Boost Date-Time
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
+// Boost Tokeniser
 #include <boost/tokenizer.hpp>
+// Boost Program Options
 #include <boost/program_options.hpp>
+// Boost Accumulators
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp>
 // StdAir
 #include <stdair/stdair_basic_types.hpp>
 #include <stdair/bom/EventStruct.hpp>
@@ -29,64 +34,54 @@
 #include <trademgen/bom/BomManager.hpp>
 #include <trademgen/config/trademgen-paths.hpp>
 
+// Aliases for namespaces
+namespace ba = boost::accumulators;
 
-// //////// Type definitions ///////
-typedef std::vector<std::string> WordList_T;
+// //////// Specific type definitions ///////
+typedef unsigned int NbOfRuns_T;
 
+/** Type definition to gather statistics. */
+typedef ba::accumulator_set<double,
+                            ba::stats<ba::tag::min, ba::tag::max,
+                                      ba::tag::mean (ba::immediate),
+                                      ba::tag::sum,
+                                      ba::tag::variance> > stat_acc_type;
 
 // //////// Constants //////
 /** Default name and location for the log file. */
-const std::string K_TRADEMGEN_DEFAULT_LOG_FILENAME ("trademgen.log");
+const stdair::Filename_T K_TRADEMGEN_DEFAULT_LOG_FILENAME ("trademgen.log");
 
 /** Default name and location for the (CSV) input file. */
-const std::string K_TRADEMGEN_DEFAULT_INPUT_FILENAME (STDAIR_SAMPLE_DIR "/demand01.csv");
+const stdair::Filename_T K_TRADEMGEN_DEFAULT_INPUT_FILENAME (STDAIR_SAMPLE_DIR
+                                                             "/demand01.csv");
 
 /** Default name and location for the (CSV) output file. */
-const std::string K_TRADEMGEN_DEFAULT_OUTPUT_FILENAME ("request.csv");
+const stdair::Filename_T K_TRADEMGEN_DEFAULT_OUTPUT_FILENAME ("request.csv");
 
-/** Default query string. */
-const std::string K_TRADEMGEN_DEFAULT_QUERY_STRING ("my good old query");
+/** Default number of random draws to be generated (best if over 100). */
+const NbOfRuns_T K_TRADEMGEN_DEFAULT_RANDOM_DRAWS = 100;
 
 
-// //////////////////////////////////////////////////////////////////////
-void tokeniseStringIntoWordList (const std::string& iPhrase,
-                                 WordList_T& ioWordList) {
-  // Empty the word list
-  ioWordList.clear();
+/** Display the statistics held by the dedicated accumulator. */
+void stat_display (std::ostream& oStream, const stat_acc_type& iStatAcc) {
+
+  // Store current formatting flags of the output stream
+  std::ios::fmtflags oldFlags = oStream.flags();
+
+  //
+  oStream.setf (std::ios::fixed);
   
-  // Boost Tokeniser
-  typedef boost::tokenizer<boost::char_separator<char> > Tokeniser_T;
+  //
+  oStream << "Statistics for the demand generation runs: " << std::endl;
+  oStream << "  minimum   = " << ba::min (iStatAcc) << std::endl;
+  oStream << "  mean      = " << ba::mean (iStatAcc) << std::endl;
+  oStream << "  maximum   = " << ba::max (iStatAcc) << std::endl;
+  oStream << "  count     = " << ba::count (iStatAcc) << std::endl;
+  oStream << "  variance  = " << ba::variance (iStatAcc) << std::endl;
   
-  // Define the separators
-  const boost::char_separator<char> lSepatorList(" .,;:|+-*/_=!@#$%`~^&(){}[]?'<>\"");
-  
-  // Initialise the phrase to be tokenised
-  Tokeniser_T lTokens (iPhrase, lSepatorList);
-  for (Tokeniser_T::const_iterator tok_iter = lTokens.begin();
-       tok_iter != lTokens.end(); ++tok_iter) {
-    const std::string& lTerm = *tok_iter;
-    ioWordList.push_back (lTerm);
-  }
-  
+  // Reset formatting flags of output stream
+  oStream.flags (oldFlags);
 }
-
-// //////////////////////////////////////////////////////////////////////
-std::string createStringFromWordList (const WordList_T& iWordList) {
-  std::ostringstream oStr;
-
-  unsigned short idx = iWordList.size();
-  for (WordList_T::const_iterator itWord = iWordList.begin();
-       itWord != iWordList.end(); ++itWord, --idx) {
-    const std::string& lWord = *itWord;
-    oStr << lWord;
-    if (idx > 1) {
-      oStr << " ";
-    }
-  }
-  
-  return oStr.str();
-}
-
 
 // ///////// Parsing of Options & Configuration /////////
 // A helper function to simplify the main part.
@@ -100,20 +95,10 @@ template<class T> std::ostream& operator<< (std::ostream& os,
 const int K_TRADEMGEN_EARLY_RETURN_STATUS = 99;
 
 /** Read and parse the command line options. */
-int readConfiguration (int argc, char* argv[], 
-                       std::string& ioQueryString,
+int readConfiguration (int argc, char* argv[], NbOfRuns_T& ioRandomRuns,
                        stdair::Filename_T& ioInputFilename,
                        stdair::Filename_T& ioOutputFilename,
-                       std::string& ioLogFilename) {
-
-  // Initialise the travel query string, if that one is empty
-  if (ioQueryString.empty() == true) {
-    ioQueryString = K_TRADEMGEN_DEFAULT_QUERY_STRING;
-  }
-  
-  // Transform the query string into a list of words (STL strings)
-  WordList_T lWordList;
-  tokeniseStringIntoWordList (ioQueryString, lWordList);
+                       stdair::Filename_T& ioLogFilename) {
 
   // Declare a group of options that will be allowed only on command line
   boost::program_options::options_description generic ("Generic options");
@@ -126,6 +111,9 @@ int readConfiguration (int argc, char* argv[],
   // line and in config file
   boost::program_options::options_description config ("Configuration");
   config.add_options()
+    ("draws,d",
+     boost::program_options::value<NbOfRuns_T>(&ioRandomRuns)->default_value(K_TRADEMGEN_DEFAULT_RANDOM_DRAWS), 
+     "Number of runs for the demand generations")
     ("input,i",
      boost::program_options::value< std::string >(&ioInputFilename)->default_value(K_TRADEMGEN_DEFAULT_INPUT_FILENAME),
      "(CVS) input file for the demand distributions")
@@ -135,9 +123,6 @@ int readConfiguration (int argc, char* argv[],
     ("log,l",
      boost::program_options::value< std::string >(&ioLogFilename)->default_value(K_TRADEMGEN_DEFAULT_LOG_FILENAME),
      "Filepath for the logs")
-    ("query,q",
-     boost::program_options::value< WordList_T >(&lWordList)->multitoken(),
-     "Query word list")
     ;
 
   // Hidden options, will be allowed both on command line and
@@ -187,21 +172,21 @@ int readConfiguration (int argc, char* argv[],
 
   if (vm.count ("input")) {
     ioInputFilename = vm["input"].as< std::string >();
-    std::cout << "Input filename is: " << ioInputFilename << std::endl;
   }
 
   if (vm.count ("output")) {
     ioOutputFilename = vm["output"].as< std::string >();
-    std::cout << "Output filename is: " << ioOutputFilename << std::endl;
   }
 
   if (vm.count ("log")) {
     ioLogFilename = vm["log"].as< std::string >();
-    std::cout << "Log filename is: " << ioLogFilename << std::endl;
   }
 
-  ioQueryString = createStringFromWordList (lWordList);
-  std::cout << "The query string is: " << ioQueryString << std::endl;
+  //
+  std::cout << "The number of runs is: " << ioRandomRuns << std::endl;
+  std::cout << "Input filename is: " << ioInputFilename << std::endl;
+  std::cout << "Output filename is: " << ioOutputFilename << std::endl;
+  std::cout << "Log filename is: " << ioLogFilename << std::endl;
   
   return 0;
 }
@@ -209,8 +194,11 @@ int readConfiguration (int argc, char* argv[],
 // /////////////// M A I N /////////////////
 int main (int argc, char* argv[]) {
 
-  // Query
-  std::string lQuery;
+  // Number of random draws to be generated (best if greater than 100)
+  NbOfRuns_T lNbOfRuns;
+    
+  // Initialise the statistics collector/accumulator
+  stat_acc_type lStatAccumulator;
 
   // Input file name
   stdair::Filename_T lInputFilename;
@@ -219,19 +207,16 @@ int main (int argc, char* argv[]) {
   stdair::Filename_T lOutputFilename;
 
   // Output log File
-  std::string lLogFilename;
+  stdair::Filename_T lLogFilename;
 
   // Call the command-line option parser
   const int lOptionParserStatus = 
-    readConfiguration (argc, argv, lQuery, lInputFilename,
+    readConfiguration (argc, argv, lNbOfRuns, lInputFilename,
                        lOutputFilename, lLogFilename);
 
   if (lOptionParserStatus == K_TRADEMGEN_EARLY_RETURN_STATUS) {
     return 0;
   }
-
-  // Number of generations.
-  const int lNbOfRuns = 10;
 
   // Open and clean the .csv output file
   std::ofstream output;
@@ -251,53 +236,127 @@ int main (int argc, char* argv[]) {
   // DEBUG
   //return 0;
 
-  for (int i = 1; i <= lNbOfRuns; ++i) {
+  for (NbOfRuns_T runIdx = 1; runIdx <= lNbOfRuns; ++runIdx) {
     // /////////////////////////////////////////////////////
-    output << "Generation number " << i << std::endl;
+    output << "Run number: " << runIdx << std::endl;
+    
     // Event queue
     stdair::EventQueue lEventQueue = stdair::EventQueue ();
-    // Browse the list of DemandStreams and Generate the first event for each
-    // DemandStream.
+     
+    /**
+       Initialisation step.
+       <br>Generate the first event for each demand stream.
+    */
     trademgenService.generateFirstRequests (lEventQueue);
       
-    // Pop requests, get type, and generate next request of same type
+    /**
+       Main loop.
+       <ul>
+       <li>Pop a request and get its associated type/demand stream.</li>
+       <li>Generate the next request for the same type/demand stream.</li>
+       </ul>
+    */
+    stdair::Count_T eventIdx = 1;
     while (lEventQueue.isQueueDone() == false) {
-      stdair::EventStruct& lEventStruct = lEventQueue.popEvent ();
+
+      // Get the next event from the event queue
+      stdair::EventStruct& lEventStruct = lEventQueue.popEvent();
+      
+      // Extract the corresponding demand/booking request
       const stdair::BookingRequestStruct& lPoppedRequest =
-        lEventStruct.getBookingRequest ();
+        lEventStruct.getBookingRequest();
         
-      // Output the request.
+      // DEBUG
+      STDAIR_LOG_DEBUG ("[" << runIdx << "][" << eventIdx
+                        << "] Poped booking request: '"
+                        << lPoppedRequest.describe() << "'.");
+    
+      // Dump the request into the dedicated CSV file
       TRADEMGEN::BomManager::csvDisplay (output, lPoppedRequest);
         
       // Retrieve the corresponding demand stream
       const stdair::DemandStreamKeyStr_T& lDemandStreamKey =
         lEventStruct.getDemandStreamKey ();
-      // generate next request
-      bool stillHavingRequestsToBeGenerated = 
-        trademgenService.stillHavingRequestsToBeGenerated(lDemandStreamKey);
-      if (stillHavingRequestsToBeGenerated) {
-        stdair::BookingRequestPtr_T lNextRequest =
-          trademgenService.generateNextRequest (lDemandStreamKey);
-        assert (lNextRequest != NULL);
+      
+      // Assess whether more events should be generated for that demand stream
+      const bool stillHavingRequestsToBeGenerated = 
+        trademgenService.stillHavingRequestsToBeGenerated (lDemandStreamKey);
 
-        stdair::Duration_T lDuration = lNextRequest->getRequestDateTime() -
-          lPoppedRequest.getRequestDateTime();
+      // Retrieve, from the demand stream, the total number of events
+      // to be generated
+      const stdair::NbOfRequests_T& lNbOfRequests = trademgenService.
+        getTotalNumberOfRequestsToBeGenerated (lDemandStreamKey);
+      
+      // DEBUG
+      STDAIR_LOG_DEBUG ("=> [" << lDemandStreamKey << "] is now processed. "
+                        << "Still generate events for that demand stream? "
+                        << stillHavingRequestsToBeGenerated);
+    
+      // If there are still events to be generated for that demand stream,
+      // generate and add them to the event queue
+      if (stillHavingRequestsToBeGenerated) {
+        
+        stdair::BookingRequestPtr_T lNextRequest_ptr =
+          trademgenService.generateNextRequest (lDemandStreamKey);
+        assert (lNextRequest_ptr != NULL);
+
+        // Sanity check
+        const stdair::Duration_T lDuration =
+          lNextRequest_ptr->getRequestDateTime()
+          - lPoppedRequest.getRequestDateTime();
         if (lDuration.total_milliseconds() < 0) {
-          STDAIR_LOG_DEBUG (lDemandStreamKey);
-          STDAIR_LOG_DEBUG (lPoppedRequest.getRequestDateTime());
-          STDAIR_LOG_DEBUG (lNextRequest->getRequestDateTime());
+          STDAIR_LOG_ERROR ("[" << lDemandStreamKey << "] The date-time of the "
+                            << "generated event ("
+                            << lNextRequest_ptr->getRequestDateTime()
+                            << ") is lower than the date-time "
+                            << "of the current event ("
+                            << lPoppedRequest.getRequestDateTime() << ")");
           assert (false);
         }
-          
-        stdair::EventStruct lNextEventStruct ("Request", lDemandStreamKey,
-                                              lNextRequest);
+
+        //
+        const stdair::EventType_T lEventTypeStr ("Request");
+        stdair::EventStruct lNextEventStruct (lEventTypeStr, lDemandStreamKey,
+                                              lNextRequest_ptr);
+
+        /**
+           Note that when adding an event in the event queue, the
+           former can be altered. It happends when an event already
+           exists in the event queue with exactly the same date-time
+           stamp. In that case, the date-time stamp is altered for the
+           newly added event, so that the unicity on the date-time
+           stamp can be guaranteed.
+        */
         lEventQueue.addEvent (lNextEventStruct);
+
+        // DEBUG
+        STDAIR_LOG_DEBUG ("[" << lDemandStreamKey << "] Added request: '"
+                          << lNextRequest_ptr->describe()
+                          << "'. Is queue done? " << lEventQueue.isQueueDone());
       }
+    
+      /**
+         Remove the last used event, so that, at any given moment, the
+         queue keeps only the active events.
+      */
       lEventQueue.eraseLastUsedEvent ();
+
+      // Iterate
+      ++eventIdx;
     }
 
+    // Add the number of events to the statistics accumulator
+    lStatAccumulator (eventIdx);
+    
+    // Reset the service (including the event queue) for the next run
+    eventIdx = 0;
     trademgenService.reset();
   }
+
+  // DEBUG
+  std::ostringstream oStr;
+  stat_display (oStr, lStatAccumulator);
+  STDAIR_LOG_DEBUG (oStr.str());
   
   // Close the Log outputFile
   logOutputFile.close();
