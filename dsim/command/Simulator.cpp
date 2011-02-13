ae53624ed80b0c1f12457e675d59a38be4d89081
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-02-13 04:03:29 +0100

    [TraDemGen][Dev] Updated the code to the new demand-related API of StdAir.

--- dsim/command/Simulator.cpp
+++ dsim/command/Simulator.cpp
@@ -36,21 +36,39 @@ namespace DSIM {
 
       // /////////////////////////////////////////////////////
       // Event queue
-      stdair::EventQueue lEventQueue = stdair::EventQueue ();
-      // Browse the list of DemandStreams and Generate the first event for each
-      // DemandStream.
+      stdair::EventQueue lEventQueue;
+
+      /**
+         Initialisation step.
+         <br>Generate the first event for each demand stream.
+      */
       ioTRADEMGEN_Service.generateFirstRequests (lEventQueue);
   
-  
-      // Pop requests, get type, and generate next request of same type
+      /** (Boost) progress display (current number of events, total
+          number of events) for every demand stream. */
+      stdair::ProgressDisplayMap_T lProgressDisplays;
+      lEventQueue.initProgressDisplays (lProgressDisplays);
+
+      /**
+         Main loop.
+         <ul>
+         <li>Pop a request and get its associated type/demand stream.</li>
+         <li>Generate the next request for the same type/demand stream.</li>
+         </ul>
+      */
+      stdair::Count_T eventIdx = 1;
       while (lEventQueue.isQueueDone() == false) {
+
+        // Get the next event from the event queue
         stdair::EventStruct& lEventStruct = lEventQueue.popEvent ();
 
+        // Extract the corresponding demand/booking request
         const stdair::BookingRequestStruct& lPoppedRequest =
           lEventStruct.getBookingRequest ();
 
         // DEBUG
-        STDAIR_LOG_DEBUG ("Poped request: " << lPoppedRequest.describe());
+        STDAIR_LOG_DEBUG ("[" << eventIdx << "] Poped booking request: '"
+                          << lPoppedRequest.describe() << "'.");
     
         // Play booking request
         playBookingRequest (ioSIMCRS_Service, lPoppedRequest);
@@ -58,19 +76,73 @@ namespace DSIM {
         // Retrieve the corresponding demand stream
         const stdair::DemandStreamKeyStr_T& lDemandStreamKey =
           lEventStruct.getDemandStreamKey ();
-        // generate next request
-        bool stillHavingRequestsToBeGenerated = 
-          ioTRADEMGEN_Service.stillHavingRequestsToBeGenerated(lDemandStreamKey);
+
+        // Assess whether more events should be generated for that
+        // demand stream
+        bool stillHavingRequestsToBeGenerated = ioTRADEMGEN_Service.
+          stillHavingRequestsToBeGenerated (lDemandStreamKey);
+        
+        // Retrieve, from the demand stream, the total number of events
+        // to be generated
+        const stdair::NbOfRequests_T& lNbOfRequests = ioTRADEMGEN_Service.
+          getTotalNumberOfRequestsToBeGenerated (lDemandStreamKey);
+      
+        // DEBUG
+        STDAIR_LOG_DEBUG ("=> [" << lDemandStreamKey << "] is now processed. "
+                          << "Still generate events for that demand stream? "
+                          << stillHavingRequestsToBeGenerated);
+    
+        // If there are still events to be generated for that demand
+        // stream, generate and add them to the event queue
         if (stillHavingRequestsToBeGenerated) {
-          stdair::BookingRequestPtr_T lNextRequest =
+          stdair::BookingRequestPtr_T lNextRequest_ptr =
             ioTRADEMGEN_Service.generateNextRequest (lDemandStreamKey);
-          assert (lNextRequest != NULL);
+          assert (lNextRequest_ptr != NULL);
           
-          stdair::EventStruct lNextEventStruct ("Request", lDemandStreamKey,
-                                                lNextRequest);
+          // Sanity check
+          const stdair::Duration_T lDuration =
+            lNextRequest_ptr->getRequestDateTime()
+            - lPoppedRequest.getRequestDateTime();
+          if (lDuration.total_milliseconds() < 0) {
+            STDAIR_LOG_ERROR ("[" << lDemandStreamKey
+                              << "] The date-time of the generated event ("
+                              << lNextRequest_ptr->getRequestDateTime()
+                              << ") is lower than the date-time "
+                              << "of the current event ("
+                              << lPoppedRequest.getRequestDateTime() << ")");
+            assert (false);
+          }
+
+          //
+          stdair::EventStruct lNextEventStruct (stdair::EventType::BKG_REQ,
+                                                lDemandStreamKey,
+                                                lNextRequest_ptr);
+
+          /**
+             Note that when adding an event in the event queue, the
+             former can be altered. It happends when an event already
+             exists in the event queue with exactly the same date-time
+             stamp. In that case, the date-time stamp is altered for the
+             newly added event, so that the unicity on the date-time
+             stamp can be guaranteed.
+          */
           lEventQueue.addEvent (lNextEventStruct);
+
+          // DEBUG
+          STDAIR_LOG_DEBUG ("[" << lDemandStreamKey << "] Added request: '"
+                            << lNextRequest_ptr->describe()
+                            << "'. Is queue done? "
+                            << lEventQueue.isQueueDone());
         }
+        
+        /**
+           Remove the last used event, so that, at any given moment, the
+           queue keeps only the active events.
+        */
         lEventQueue.eraseLastUsedEvent ();
+
+        // Iterate
+        ++eventIdx;
       }
             
       // DEBUG
