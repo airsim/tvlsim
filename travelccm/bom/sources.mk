e29b9eb7f09fc7f7ffd2ce74051136e4bb6c9c24
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-03-04 16:33:42 +0100

    [TravelCCM] Suppressed useless BomAbract files.

--- travelccm/bom/sources.mk
+++ travelccm/bom/sources.mk
@@ -1,6 +1,4 @@
 travelccm_bom_h_sources = \
-	$(top_srcdir)/travelccm/bom/BomAbstract.hpp \
 	$(top_srcdir)/travelccm/bom/PriceOrientedModel.hpp
 travelccm_bom_cc_sources = \
-	$(top_srcdir)/travelccm/bom/BomAbstract.cpp \
 	$(top_srcdir)/travelccm/bom/PriceOrientedModel.cpp
