e651ba7bc5688820307e0ceef36bf20e20addf48
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-04-17 21:08:41 +0200

    [SimCRS][Dev] Adapted the service implementation to the new simplified inventory service API.

--- simcrs/service/SIMCRS_Service.cpp
+++ simcrs/service/SIMCRS_Service.cpp
@@ -302,9 +302,11 @@ namespace SIMCRS {
      * longer referenced (e.g., at the end of the process).
      */
     AIRINV_Master_ServicePtr_T lAIRINV_Master_Service_ptr =
-      boost::make_shared<AIRINV::AIRINV_Master_Service> (lSTDAIR_Service_ptr,
-                                                         iScheduleInputFilename,
-                                                         iODInputFilename);
+      boost::make_shared<AIRINV::AIRINV_Master_Service> (lSTDAIR_Service_ptr);
+
+    // Parse and load the schedule and O&D input files
+    lAIRINV_Master_Service_ptr->parseAndLoad (iScheduleInputFilename,
+                                              iODInputFilename);
 
     // Store the Airinv service object within the (SimCRS) service context
     lSIMCRS_ServiceContext.setAIRINV_Master_Service(lAIRINV_Master_Service_ptr);
