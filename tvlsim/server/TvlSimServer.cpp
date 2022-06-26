fd9d909e54f53db163499170d7c506261ad1aa18
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2022-06-26 22:57:39 +0200

    [Dev] Switched to newer ZeroMQ C++ API

--- tvlsim/server/TvlSimServer.cpp
+++ tvlsim/server/TvlSimServer.cpp
@@ -511,7 +511,7 @@ int readConfiguration (int argc, char* argv[], std::string& ioServerProtocol,
  */
 static std::string s_recv (zmq::socket_t& socket) {
   zmq::message_t message;
-  socket.recv (&message);
+  socket.recv (message);
 
   return std::string (static_cast<char*> (message.data()), message.size());
 }
@@ -519,11 +519,11 @@ static std::string s_recv (zmq::socket_t& socket) {
 /**
  * Convert string to 0MQ string and send to socket.
  */
-static bool s_send (zmq::socket_t& socket, const std::string& string) {
+static zmq::send_result_t s_send (zmq::socket_t& socket, const std::string& string) {
   zmq::message_t message (string.size());
   memcpy (message.data(), string.data(), string.size());
 
-  bool rc = socket.send (message);
+  zmq::send_result_t rc = socket.send (message, zmq::send_flags::dontwait);
   return rc;
 }
 
